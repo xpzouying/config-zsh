cc253fd646fb0465c6c5575f81583d2840f101ad
Zou, Ying <xpzouying@gmail.com>
2016-06-02 11:55:27 +0800

    Add some plugin: autojump, dircolor...

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # config-zsh
 
 Config zshrc.
+
+
+##########
+# color for dircolors
+
+$ brew install coreutils
+$ git clone https://github.com/seebi/dircolors-solarized.git
+$ mv ./dircolors-solarized/dircolors.ansi-universal ~/.dircolors-solarized
+
+cat ~/.bash_profile
+    eval "$(gdircolors ~/.dircolors-solarized)"
+    alias ls='gls --color=auto'
+
--- install.sh
+++ install.sh
@@ -13,9 +13,18 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 
 
 # 2. Plugins
+
+# tmux-plugins
+echo "Install tmux-plugins:"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+
 if [[ "$OS" == "Linux" ]]; then
+    echo "OS is Linux"
     
 elif [[ "$OS" == "Darwin" ]]; then
+    echo "OS is Mac"
     # Mac OSX
     
     # fzf
@@ -23,7 +32,25 @@ elif [[ "$OS" == "Darwin" ]]; then
     # install fzf shell extensions
     /usr/local/opt/fzf/install
 
+    # dircolor
+    brew install coreutils
+    cp $(pwd)/config/dircolors/dircolors.256dark ~/.dircolors-solarized
+    echo "eval \"$(gdircolors ~/.dircolors-solarized)\"" >> ~/.zshrc
+    echo "alias ls='gls --color=auto'" >> ~/.zshrc
+
+    # oh-my-zsh plugin
+    # autojump
+    brew install autojump
+
+    echo "[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
+
 else
     echo "Unknown system."
     exit 1
 fi
+
+
+
+# Config
+echo "Config Tmux:"
+ln -sf $(pwd)/config/tmux/tmux.conf $HOME/.tmux.conf
